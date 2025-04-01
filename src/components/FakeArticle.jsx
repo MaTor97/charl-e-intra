b356bc663843bb510bd644d690b938c3e8e3fd79
MaTor97 <mttortora97@gmail.com>
2025-04-01 11:21:57 +0200

    article image size

--- src/components/FakeArticle.jsx
+++ src/components/FakeArticle.jsx
@@ -6,7 +6,7 @@ const Article = () => {
         <main>
             <h1>La révolution du télétravail</h1>
             <div className="imageContainer">
-
+                <img src={ArticleImage} alt="Télétravail" />
             </div>
             <p>Depuis la pandémie, le télétravail est devenu une norme dans de nombreuses entreprises. Flexibilité, réduction des trajets et meilleur équilibre entre vie privée et professionnelle sont les principaux avantages. Toutefois, la frontière entre travail et repos devient floue, et l'isolement social peut peser sur les employés. Les entreprises cherchent encore le bon équilibre entre présence physique et télétravail.</p>
         </main>
