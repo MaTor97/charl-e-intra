dba0039e20bbbf0efd1ab9fedf064376798799c9
MaTor97 <mttortora97@gmail.com>
2025-04-01 10:50:54 +0200

    tenter une approche avec display au lieu de transform

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,12 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import logo from "../assets/images/C_logo_black.png";
 import { DarkModeSVG, SearchSVG } from "../assets/files/SVG";
 
 const Header = () => {
-    const [hidden, setHidden] = useState(false);
-    const lastScrollY = useRef(0); // Utilisation de useRef
-
-    useEffect(() => {
-        const handleScroll = () => {
-            const currentScrollY = window.scrollY;
-
-            if (currentScrollY > lastScrollY.current + 10) {
-                setHidden(true); // Scroll vers le bas
-            } else if (currentScrollY < lastScrollY.current - 10) {
-                setHidden(false); // Scroll vers le haut
-            }
-
-            lastScrollY.current = currentScrollY; // Met à jour la position
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+    
 
     return (
-        <header className={hidden ? "hidden" : ""}>
+        <header>
             <div className="logoMode">
                 <img src={logo} alt="Logo" className="logo"/>
                 <DarkModeSVG />
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 
 const Nav = () => {
-    const [hidden, setHidden] = useState(false);
-    const lastScrollY = useRef(0); // Utilisation de useRef
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const currentScrollY = window.scrollY;
-
-            if (currentScrollY > lastScrollY.current + 10) {
-                setHidden(true); // Scroll vers le bas
-            } else if (currentScrollY < lastScrollY.current - 10) {
-                setHidden(false); // Scroll vers le haut
-            }
-
-            lastScrollY.current = currentScrollY; // Met à jour la position
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
 
     return (
-        <nav className={hidden ? "hidden" : ""}>
+        <nav>
             <ul>
                 <li>NEWS</li>
                 <li>APPELS ET PROMOTIONS</li>
