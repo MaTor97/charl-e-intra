c747e98fd1f5ab5afb99e6055c524da76a433c70
MaTor97 <mttortora97@gmail.com>
2025-04-01 10:53:45 +0200

    tenter une approche avec display au lieu de transform

--- src/components/FakeArticle.jsx
+++ src/components/FakeArticle.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Article = () => {
-    <main>
-        <h1>La révolution du télétravail</h1>
-        <img src="../assets/images/remote.jpg" alt="Télétravail" className="articleImage"/>
-        <p>Depuis la pandémie, le télétravail est devenu une norme dans de nombreuses entreprises. Flexibilité, réduction des trajets et meilleur équilibre entre vie privée et professionnelle sont les principaux avantages. Toutefois, la frontière entre travail et repos devient floue, et l'isolement social peut peser sur les employés. Les entreprises cherchent encore le bon équilibre entre présence physique et télétravail.</p>
-    </main>
+    return (
+        <main>
+            <h1>La révolution du télétravail</h1>
+            <img src="../assets/images/remote.jpg" alt="Télétravail" className="articleImage"/>
+            <p>Depuis la pandémie, le télétravail est devenu une norme dans de nombreuses entreprises. Flexibilité, réduction des trajets et meilleur équilibre entre vie privée et professionnelle sont les principaux avantages. Toutefois, la frontière entre travail et repos devient floue, et l'isolement social peut peser sur les employés. Les entreprises cherchent encore le bon équilibre entre présence physique et télétravail.</p>
+        </main>
+    );
 }
 
 export default Article;
