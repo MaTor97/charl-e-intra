6f4aa76dd5bd1e6e50a869396d375de99e3f2050
MaTor97 <mttortora97@gmail.com>
2025-04-01 09:19:29 +0200

    ajustement margin footer + header position

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../assets/images/C_logo_black.png";
 import { DarkModeSVG, SearchSVG } from "../assets/files/SVG";
 
 const Header = () => {
+    const [hidden, setHidden] = useState(false);
+    let lastScrollY = 0;
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+
+            if (currentScrollY > lastScrollY) {
+                setHidden(true);  // Scroll vers le bas → Cacher le header
+            } else {
+                setHidden(false); // Scroll vers le haut → Réafficher le header
+            }
+
+            lastScrollY = currentScrollY;
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (  
-        <header>
+        <header className={hidden ? "hidden" : ""}>
             <div className="logoMode">
                 <img src={logo} alt="Logo" className="logo"/>
                 <DarkModeSVG />
