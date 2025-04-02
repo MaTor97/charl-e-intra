cfbace5828ed4749fa86c119cb409669898751c3
MaTor97 <mttortora97@gmail.com>
2025-04-02 11:56:28 +0200

    modifs footer menu

--- src/assets/files/SVG.jsx
+++ src/assets/files/SVG.jsx
@@ -125,8 +125,8 @@ export const LightModeSVG = () => {
     return (
         <svg 
             xmlns="http://www.w3.org/2000/svg" 
-            width="48" 
-            height="48" 
+            width="28" 
+            height="28" 
             viewBox="0 0 48 48" 
             fill="none"
             >
