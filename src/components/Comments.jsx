d635364d11fef41bd176cd45ff0877f6907945ef
TORTORA Matteo <TORTMA473@acc-vdc.be>
2025-04-16 15:42:08 +0200

    enlever commentaires des endroits inutiles

---
+++ src/components/Comments.jsx
@@ -0,0 +1,49 @@
+import { fetchURL } from '../assets/files/functions/fetch';
+import { useEffect, useState } from 'react';
+import parse from 'html-react-parser';
+
+const Comments = ({ postId }) => {
+      const [comments, setComments] = useState([]);
+    
+      useEffect(() => {
+        const loadPost = async () => {
+          try {
+            const commentsData = await fetchURL(`comments?post=${postId}`);
+            setComments(commentsData);
+          } catch (error) {
+            console.error("Erreur de récupération du post:", error);
+            setComments(null);
+          }
+        };
+      
+        loadPost();
+      }, [postId]);
+      
+    
+      return (
+        <div className="commentBox">
+        <p id='title'>Commentaires</p>
+        {comments.map((comment) => {
+          const avatarUrl = Object.values(comment.author_avatar_urls).pop();
+            return (
+                <div className="comment" key={comment.id}>
+                    <img src={avatarUrl} alt="avatar" />
+                    <div className="comContent">
+                        <div className="nameNDate">
+                            <strong className="author-name">{comment.author_name}</strong>
+                            <div className="timestamp">
+                                {new Date(comment.date).toLocaleString()}
+                            </div>
+                        </div>
+                        <span>{parse(comment.content.rendered)}</span>
+                    </div>
+                </div>
+            )
+        })}
+        <textarea placeholder='Laissez un commentaire...'></textarea>
+        <button>publier</button>
+      </div>
+      );  
+}
+
+export default Comments
--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -3,20 +3,18 @@ import { useParams } from 'react-router-dom';
 import parse from 'html-react-parser';
 import { htmlParserOptions } from '../assets/files/options';
 import { fetchURL } from '../assets/files/functions/fetch';
+import Comments from '../components/Comments';
 import { stripHtml, decodeHtml } from '../assets/files/functions/cleanHTML';
 
 const Article = () => {
   const { postId } = useParams();
   const [post, setPost] = useState(null);
-  const [comments, setComments] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loadPost = async () => {
       try {
         const data = await fetchURL(`posts/${postId}`);
-        const commentsData = await fetchURL(`comments?post=${postId}`);
-        setComments(commentsData);
         setPost(data);
       } catch (error) {
         console.error("Erreur de récupération du post:", error);
@@ -42,31 +40,7 @@ const Article = () => {
       <h1>{stripHtml(decodeHtml(post.title.rendered))}</h1>
       <div>{parse(post.content.rendered, htmlParserOptions)}</div>
       <p id="date">{post.date}</p>
-
-      <div className="commentBox">
-        <p id='title'>Commentaires</p>
-        {comments.map((comment) => {
-          const avatarUrl = Object.values(comment.author_avatar_urls).pop();
-
-          return (
-            <div className="comment" key={comment.id}>
-              <img src={avatarUrl} alt="avatar" />
-              <div className="comContent">
-                <div className="nameNDate">
-                  <strong className="author-name">{comment.author_name}</strong>
-                  <div className="timestamp">
-                    {new Date(comment.date).toLocaleString()}
-                  </div>
-                </div>
-                <span>{parse(comment.content.rendered)}</span>
-              </div>
-            </div>
-          );
-        })}
-        <textarea placeholder='Laissez un commentaire...'></textarea>
-        <button>publier</button>
-      </div>
-      
+      {post.categories[1] === 54 ? <Comments postId={postId} /> : null}
     </main>
   );
 };
