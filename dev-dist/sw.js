17aea70101b761716ea103b17c895213e0ef1b30
TORTORA Matteo <TORTMA473@acc-vdc.be>
2025-04-28 09:48:05 +0200

    search tool size

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/offline.html",
-    "revision": "0.59elv7be2e8"
+    "revision": "0.b791j2nu1jg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/offline.html"), {
